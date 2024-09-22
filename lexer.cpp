b9bae17df4e486ce2e57e5125eb6348952734b14
jojoao52213 <120279168+jojoao52213@users.noreply.github.com>
2024-09-22 20:55:09 -0300

    Update lexer.cpp

--- lexer.cpp
+++ lexer.cpp
@@ -7,20 +7,30 @@ using namespace std;
 
 enum ExpectedToken
 {
-    INDENTIFIER,
-    EXPECTING_SEMI,
+    __NULL,
+
+    _INDENTIFIER,
+    _SEMI,
+
+    _OPEN_PARENTHESES,
+    _CLOSE_PARENTHESES,
+    _EQU,
 };
 
 enum TokenType
 {
+    OPEN_CURLY_BRACKETS,
+    CLOSE_CURLY_BRACKETS,
+    OPEN_PARENTHESES,
+    CLOSE_PARENTHESES,
+    EQU,
+
     _NULL,
 
-    VAR_DEF,
+    INDENTIFIER,
 
     INT_LIT,
     INT_DEF,
-    
-    FUNC_DEF,
 };
 
 string TokenTypeToString(TokenType type)
@@ -28,18 +38,29 @@ string TokenTypeToString(TokenType type)
     switch (type)
     {
     case _NULL:
-        return "_NULL";
-    case VAR_DEF:
-        return "VAR_DEF";
+        return "_NULL ";
+    case INDENTIFIER:
+        return "INDENTIFIER ";
     case INT_LIT:
-        return "INT_LIT";
+        return "INT_LIT ";
     case INT_DEF:
-        return "INT_DEF";
-    case FUNC_DEF:
-        return "FUNC_DEF";
+        return "INT_DEF ";
+    case EQU:
+        return "EQU ";
+    case OPEN_PARENTHESES:
+        return "OPEN_PARENTHESES ";
+    case CLOSE_PARENTHESES:
+        return "CLOSE_PARENTHESES ";
+    case OPEN_CURLY_BRACKETS:
+        return "OPEN_CURLY_BRACKETS ";
+    case CLOSE_CURLY_BRACKETS:
+        return "CLOSE_CURLY_BRACKETS ";
+    default:
+        return "";
     break;
     }
-}
+    return "";
+};
 
 struct Token
 {
@@ -50,24 +71,101 @@ struct Token
 vector<Token> lex(string text)
 {
     vector<Token> TokensStream;
+
+    // Token Buffer initialization
     Token TokenBuffer;
     TokenBuffer.type = _NULL;
-    string StringBuffer;
-    ExpectedToken expecting;
 
-    for(char CURRENT_CHAR : text)
+    string WithOutSpaceCode;
+
+    for (char c : text)
+    {
+        if (c != '\t' & c != '\n' & c != ' ')
+        {
+            WithOutSpaceCode += c;
+        }
+    }
+
+    // Lexer Main part initialization
+    string StringBuffer;
+    ExpectedToken expected;
+    bool IndentifierInProgress = false;
+    string IndentifierBuffer;
+
+    // Lexer Main part
+    for (char CURRENT_CHAR : WithOutSpaceCode)
     {
         StringBuffer += CURRENT_CHAR;
-        if(StringBuffer == "int")
+
+        if(expected == _INDENTIFIER)
         {
+            if(IndentifierInProgress & CURRENT_CHAR != '=' & CURRENT_CHAR != '(')
+            {
+                IndentifierBuffer += CURRENT_CHAR;
+            }
+            else
+            {
+                TokenBuffer.type = INDENTIFIER;
+                TokenBuffer.value = IndentifierBuffer;
+                TokensStream.push_back(TokenBuffer);
+                TokenBuffer.type = _NULL;
+                TokenBuffer.value = "";
+
+                IndentifierBuffer = "";
+                IndentifierInProgress = false;
+                expected = __NULL;
+
+                if(CURRENT_CHAR == '=')
+                {
+                    TokenBuffer.type = EQU;
+                    TokensStream.push_back(TokenBuffer);
+                    TokenBuffer.type = _NULL;
+                }
+                else if(CURRENT_CHAR == '(')
+                {
+                    TokenBuffer.type = OPEN_PARENTHESES;
+                    TokensStream.push_back(TokenBuffer);
+                    TokenBuffer.type = _NULL;
+                }
+
+                StringBuffer = "";
+            }
+        }
+
+        if(StringBuffer == "int" & expected != _INDENTIFIER)
+        {
+            StringBuffer = "";
             TokenBuffer.type = INT_DEF;
             TokensStream.push_back(TokenBuffer);
             TokenBuffer.type = _NULL;
+            expected = _INDENTIFIER;
+            IndentifierInProgress = true;
+        }
+
+        if(StringBuffer == ")")
+        {
+           TokenBuffer.type = CLOSE_PARENTHESES;
+           TokensStream.push_back(TokenBuffer);
+           TokenBuffer.type = _NULL; 
+           StringBuffer = "";
+        }
+    
+        if(StringBuffer == "{")
+        {
+            TokenBuffer.type = OPEN_CURLY_BRACKETS;
+            TokensStream.push_back(TokenBuffer);
+            TokenBuffer.type = _NULL;
             StringBuffer = "";
-            expecting = INDENTIFIER;
-        };
+        }
 
-    };  
+        if(StringBuffer == "}")
+        {
+            TokenBuffer.type = CLOSE_CURLY_BRACKETS;
+            TokensStream.push_back(TokenBuffer);
+            TokenBuffer.type = _NULL;
+            StringBuffer = "";
+        }
+    }
 
-    return TokensStream;   
+    return TokensStream;
 };
